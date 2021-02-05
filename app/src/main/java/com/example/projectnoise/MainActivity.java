5f96870bd6bd7cc32441c0572c8018be8aa457fc
Mihir <mihir.bhalodia@uconn.edu>
2021-02-05 00:02:00 -0500

    started working on notification

--- app/src/main/java/com/example/projectnoise/MainActivity.java
+++ app/src/main/java/com/example/projectnoise/MainActivity.java
@@ -3,16 +3,15 @@ package com.example.projectnoise;
 import android.Manifest;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
-import android.content.BroadcastReceiver;
-import android.content.Context;
+import android.app.PendingIntent;
 import android.content.Intent;
-import android.content.IntentFilter;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
+import androidx.core.app.NotificationCompat;
 import androidx.core.content.ContextCompat;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
@@ -55,6 +54,28 @@ public class MainActivity extends AppCompatActivity {
         NotificationManager notificationManager = this.getSystemService(NotificationManager.class);
         notificationManager.createNotificationChannel(channel);
     }
+
+    // Creates and displays a notification
+    private void addNotification() {
+        // Builds your notification
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, null)
+                .setSmallIcon(R.mipmap.ic_launcher_round)
+                .setContentTitle("Project Noise")
+                .setContentText("Db level exceed!");
+
+        // Creates the intent needed to show the notification
+        Intent notificationIntent = new Intent(this, MainActivity.class);
+        PendingIntent contentIntent = PendingIntent.getActivity(this, 0, notificationIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+        builder.setContentIntent(contentIntent);
+
+        // Add as notification
+        NotificationManager manager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        manager.notify(0, builder.build());
+    }
+
+
+
+
 
 
     /**
