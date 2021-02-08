5594555af595cbe9cfde0392f00fac26b9ad40a5
Luca Alberti <48816961+imabigdipper@users.noreply.github.com>
2021-02-08 17:47:08 -0500

    Added basic settings menu with support for setting calibration and start/end time for MeasureService. Only calibration works atm

--- app/src/main/java/com/example/projectnoise/MainActivity.java
+++ app/src/main/java/com/example/projectnoise/MainActivity.java
@@ -3,10 +3,6 @@ package com.example.projectnoise;
 import android.Manifest;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
-import android.content.BroadcastReceiver;
-import android.content.Context;
-import android.content.Intent;
-import android.content.IntentFilter;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.util.Log;
@@ -34,7 +30,7 @@ public class MainActivity extends AppCompatActivity {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
+                R.id.navigation_home, R.id.navigation_preferences, R.id.navigation_notifications)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
