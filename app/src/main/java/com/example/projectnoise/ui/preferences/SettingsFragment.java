7468070697cd3c25daf85d19cd1286e997bf9f89
Luca Alberti <48816961+imabigdipper@users.noreply.github.com>
2021-03-02 13:26:00 -0500

    Removed Home tab, moved service start/stop to Settings panel. Deleted unused fragments

--- app/src/main/java/com/example/projectnoise/ui/preferences/SettingsFragment.java
+++ app/src/main/java/com/example/projectnoise/ui/preferences/SettingsFragment.java
@@ -1,7 +1,13 @@
 package com.example.projectnoise.ui.preferences;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 
+import androidx.preference.Preference;
+import androidx.preference.PreferenceManager;
+
+import com.example.projectnoise.services.MeasureService;
 import com.takisoft.preferencex.PreferenceFragmentCompat;
 
 import com.example.projectnoise.R;
@@ -11,5 +17,30 @@ public class SettingsFragment extends PreferenceFragmentCompat {
     @Override
     public void onCreatePreferencesFix(Bundle savedInstanceState, String rootKey) {
         setPreferencesFromResource(R.xml.root_preferences, rootKey);
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstance){
+        super.onCreate(savedInstance);
+        PreferenceManager.setDefaultValues(this.getContext(), R.xml.root_preferences, false);
+        Preference start_button = findPreference("service_start");
+        start_button.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+            @Override
+            public boolean onPreferenceClick(Preference preference) {
+                Intent intent = new Intent(getActivity(), MeasureService.class);
+                getActivity().startService(intent);
+                return true;
+            }
+        });
+
+        Preference stop_button = findPreference("service_stop");
+        stop_button.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+            @Override
+            public boolean onPreferenceClick(Preference preference) {
+                Intent intent = new Intent(getActivity(), MeasureService.class);
+                getActivity().stopService(intent);
+                return true;
+            }
+        });
     }
 }
